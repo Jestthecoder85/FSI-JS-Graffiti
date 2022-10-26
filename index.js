880b12b773c65b2be2ddb0abea31e27688ade52e
Jestthecoder85 <majesticwilson85@gmail.com>
2022-10-26 00:21:36 -0400

    Updated

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 
 // Select some elements...
 let header = document.querySelector ('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll ('.dog-image')
+for(let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let pupNames = document.querySelectorAll ('.dog-name')
+for(let i = 0; i < pupNames.length; i++) {
+    pupNames[i].style.textAlign = "left"
+}
